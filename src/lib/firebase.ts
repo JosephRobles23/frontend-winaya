333dedd9a82e1964c745e3b426623a9cd000e820
Joseph Robles <peter.chuquipiondo.r@uni.pe>
2024-11-16 19:07:23 -0500

    Cambio de proyecto de Firebase Authentication

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBioduKTzpjEDqxQ7ysqRqZpyZmGZsBs5Q",
-  authDomain: "ieee-cis-uni-6b2a2.firebaseapp.com",
-  projectId: "ieee-cis-uni-6b2a2",
-  storageBucket: "ieee-cis-uni-6b2a2.appspot.com",
-  messagingSenderId: "70687061438",
-  appId: "1:70687061438:web:f4238d358740a77db635ef"
+  apiKey: "AIzaSyDOHQCCjvTkfRSgs0pURCx5-arRLmGlnrM",
+  authDomain: "winaya-tech.firebaseapp.com",
+  projectId: "winaya-tech",
+  storageBucket: "winaya-tech.firebasestorage.app",
+  messagingSenderId: "712002030080",
+  appId: "1:712002030080:web:73ad3cd3bdcc1eb7dea6e5"
 };
 
 const app = initializeApp(firebaseConfig);
